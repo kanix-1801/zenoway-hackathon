1c200db4bb35ccc7eaa8b632447b02028fcad68a
kanix-1801 <kanishk.singhal1801@gmail.com>
2023-05-13 19:57:50 +0530

    minor fix

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,57 +1,57 @@
-import Header from "../components/Header";
-import React, { useState, useEffect } from "react";
-import LeftSidebar from "../components/LeftSidebar";
-import Post from "../components/Post";
-import Contract from "../Contract.json";
-import { ethers } from "ethers";
-import { useAccount } from "wagmi";
-import RefreshIcon from "@mui/icons-material/Refresh";
-import UserProfile from "../components/UserProfile";
+import Header from '../components/Header'
+import React, { useState, useEffect } from 'react'
+import LeftSidebar from '../components/LeftSidebar'
+import Post from '../components/Post'
+import Contract from '../Contract.json'
+import { ethers } from 'ethers'
+import { useAccount } from 'wagmi'
+import RefreshIcon from '@mui/icons-material/Refresh'
+import UserProfile from '../components/UserProfile'
 
 function Profile() {
-  const [posts, setPosts] = useState([]);
-  const { address: userAddress } = useAccount();
+  const [posts, setPosts] = useState([])
+  const { address: userAddress } = useAccount()
 
   async function handleReadPost() {
-    let zenowayContract;
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    let zenowayContract
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
     const chainId = await provider
       .getNetwork()
-      .then((network) => network.chainId);
+      .then((network) => network.chainId)
 
     if (chainId === 80001) {
       zenowayContract = {
         contractAddress: Contract.MumbaiContractAddress,
         contractAbi: Contract.abi,
-      };
+      }
     } else if (chainId === 8082) {
       zenowayContract = {
         contractAddress: Contract.ShardeumContractAddress,
         contractAbi: Contract.abi,
-      };
+      }
     } else {
       zenowayContract = {
         contractAddress: Contract.SpoliaContractAddress,
         contractAbi: Contract.abi,
-      };
+      }
     }
 
     const contract = new ethers.Contract(
       zenowayContract.contractAddress,
       zenowayContract.contractAbi,
-      provider
-    );
+      provider,
+    )
 
-    const allPost = await contract.readUserPosts(userAddress);
-    setPosts(allPost);
+    const allPost = await contract.readUserPosts(userAddress)
+    setPosts(allPost)
   }
 
   useEffect(() => {
-    handleReadPost();
-  }, []);
+    handleReadPost()
+  }, [])
 
   function refresh() {
-    window.location.reload();
+    window.location.reload()
   }
 
   return (
@@ -64,14 +64,14 @@ function Profile() {
             <span onClick={refresh}>
               <RefreshIcon
                 style={{
-                  transform: "scale(2)",
-                  margin: "1.5rem",
-                  cursor: "pointer",
+                  transform: 'scale(2)',
+                  margin: '1.5rem',
+                  cursor: 'pointer',
                 }}
               />
             </span>
             <br />
-            <span style={{ cursor: "pointer" }}>No Posts Yet...</span>
+            <span style={{ cursor: 'pointer' }}>No Posts Yet...</span>
           </div>
         ) : (
           posts
@@ -87,13 +87,13 @@ function Profile() {
                   postCaption={post.postCaption}
                   dateCreated={post.timeCreated}
                 />
-              );
+              )
             })
         )}
       </div>
       <UserProfile />
     </>
-  );
+  )
 }
 
-export default Profile;
+export default Profile
